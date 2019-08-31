a5c6d39b165ac250b0f9c91c14d9de532284f89e
Marcus Vinicius <m.viniciuz@hotmail.com>
2019-08-30 21:46:34 -0300

    fix: implementando back-end - não finalizado

--- gobarber/src/Routes.js
+++ gobarber/src/Routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/user';
+
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'servidor online!' }));
+routes.get('/', async (req, res) =>{
+  const user = await User.create({
+    name: 'Marcus Vinicius',
+    email:'m.viniciuz@hotmail.com',
+    password_hash:'12345'
+  });
+   res.json({ message: 'servidor online!' })
+});
 
 export default routes;
