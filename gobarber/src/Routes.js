dee7cc1f54a1038b4c9e25254c254fc9833f4a12
Marcus Vinicius <m.viniciuz@hotmail.com>
2019-09-03 21:29:29 -0300

    Commit para o mestre Diego haha

--- gobarber/src/Routes.js
+++ gobarber/src/Routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/userController';
-
+import SessionController from './App/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
---
+++ gobarber/src/app/controllers/SessionController.js
@@ -0,0 +1,41 @@
+import jwt  from 'jsonwebtoken';
+import User from '../models/user';
+
+class SessionController {
+
+  async store(req, res){ 
+    
+    const { email, password } = req.body;    
+
+    const user = await User.findOne({ where: { email } });
+
+  //  if (!user) {
+ //     return res.status(401).json({ error: 'usuário não encontrado!'});
+ //   }
+
+  //  if (!(await user.checkPassword(req.body.password))){
+ //     return res.status(401).json({ error: 'Senha incorreta!' });
+ //   }   
+
+ //   const { id, name } = user;  
+
+ //   return res.json({
+ //     user:{
+ //       id,
+ //       name,
+ //       email
+ //     },
+
+ //     token: jwt.sign({ id }, 'c7c1213003cbd0014bebebcb588a80e9', {
+ //       expiresIn: '7d',
+ //     }),
+ 
+//    });
+
+    res.send({ email });
+  }
+}    
+
+
+
+export default new SessionController();
