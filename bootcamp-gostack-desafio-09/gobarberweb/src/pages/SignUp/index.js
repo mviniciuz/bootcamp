6006b7925de3f8e0e60844a5083a81654a2003db
Marcus Vinicius <m.viniciuz@hotmail.com>
2020-03-30 21:45:42 -0300

    fix: module 09

---
+++ bootcamp-gostack-desafio-09/gobarberweb/src/pages/SignUp/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Input, Form } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import logo from '~/assets/logo.svg';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
+
+const schema = Yup.object().shape({
+  name: Yup.string()
+    .required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Informe um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .required('A senha é obrigatória')
+    .min(6, 'Senha com no mínimo 6 digitos')
+});
+
+export default function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="gobarber" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+        <button type="submit">Criar conta </button>
+        <Link to="/">Já tenho Login</Link>
+      </Form>
+    </>
+  );
+}
